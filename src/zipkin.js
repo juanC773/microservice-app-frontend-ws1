4f0f46e6639784342728285eedbe0e6a48bd699f
Torres <alejandro.t.s.321@gmail.com>
2025-09-23 11:31:37 -0500

    fix: Change env configuration in auth and cipkin file

--- src/zipkin.js
+++ src/zipkin.js
@@ -6,7 +6,7 @@ import {
 } from 'zipkin'
 import {HttpLogger} from 'zipkin-transport-http'
 import {zipkinInterceptor} from 'zipkin-instrumentation-vue-resource'
-const ZIPKIN_URL = window.location.protocol + '//' + window.location.host + '/zipkin'
+const ZIPKIN_URL = process.env.ZIPKIN_URL || null
 /**
 * Tracing plugin that uses Zipkin. Initiates new traces with outgoing requests
 * and injects appropriate headers.
